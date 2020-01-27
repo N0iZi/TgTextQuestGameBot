749e3a19d6078d80c5abc4f9c50f4636c58446a9
Andriy Shelest <MrAShelest@gmail.com>
2020-01-27 22:01:04 +0100

    Database creation script

---
+++ materials/TGBOT.DB.sql
@@ -0,0 +1,30 @@
+CREATE TABLE [Messages] (
+  [id] int PRIMARY KEY IDENTITY(1, 1),
+  [content] nvarchar(MAX) NOT NULL,
+  [is_final] BIT NOT NULL
+)
+GO
+
+CREATE TABLE [Replies] (
+  [id] int PRIMARY KEY IDENTITY(1, 1),
+  [content] nvarchar(MAX) NOT NULL,
+  [for_message_id] int NOT NULL,
+  [next_message_id] int NOT NULL
+)
+GO
+
+CREATE TABLE [Users] (
+  [id] int PRIMARY KEY IDENTITY(1, 1),
+  [chat_id] int NOT NULL,
+  [current_message_id] int NOT NULL
+)
+GO
+
+ALTER TABLE [Replies] ADD FOREIGN KEY ([for_message_id]) REFERENCES [Messages] ([id])
+GO
+
+ALTER TABLE [Replies] ADD FOREIGN KEY ([next_message_id]) REFERENCES [Messages] ([id])
+GO
+
+ALTER TABLE [Users] ADD FOREIGN KEY ([current_message_id]) REFERENCES [Messages] ([id])
+GO
