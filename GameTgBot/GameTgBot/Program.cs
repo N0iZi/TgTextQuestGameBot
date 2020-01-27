8380b56feda1b2777be0562d7586256972ae9eb2
Andriy Shelest <MrAShelest@gmail.com>
2020-01-27 21:57:01 +0100

    Initial commit

---
+++ GameTgBot/GameTgBot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace GameTgBot
+{
+    class Program
+    {
+
+        static ITelegramBotClient botClient;
+
+        static void Main(string[] args)
+        {
+
+            botClient = new TelegramBotClient("976746314:AAHXmyYNxwZGhC50grdPCpR8PnMCJ53OchQ");
+
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+            Thread.Sleep(int.MaxValue);
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+                await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: "You said:\n" + e.Message.Text);
+            }
+        }
+    }
+}
